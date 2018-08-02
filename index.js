d0b23abc33d585053d547a180ed3923434d2f763
Bijon Miles <bijonmiles01@gmail.com>
2018-08-02 23:46:45 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ function add2(n) {
 
 var theFunk = funkyFunction;
 
-var theFunk = function funkyFunction() {
+var funkyFunction = function theFunk() {
   return function InFunction() {
     return 'FUNKY!'
   }  
